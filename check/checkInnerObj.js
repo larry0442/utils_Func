56e937738ef58b0581479da6f660a49509e6027a
yangxinchi <yangxinchi@3vjia.com>
2020-05-07 11:49:02 +0800

    feat : 四种检测类型的方法

--- check/checkInnerObj.js
+++ check/checkInnerObj.js
@@ -14,7 +14,8 @@ const Type = (function () {
       'Object',
       'Function',
       'Array',
-      'Date'
+      'Date',
+      'RegExp'
   ];
 
   for (let type of supperTypes) {
@@ -25,3 +26,31 @@ const Type = (function () {
 
   return types;
 })();
+
+// console.log(Type.isRegExp(/s+/))
+
+/* 
+① typeof 运算符判断类型，局限性在 typeof undefined === 'undefined' ,但是 null, RegExp 以及 Date，Error 这些类型的检测出来都是 object
+
+② instanceof 检测的是原型，判断检测值是否为被检测值的实例
+
+③ Object.prototype.call()，是 Object 的原型方法，所以
+
+检测返回的是 [object Number] 类似的结果
+
+④ constructor 属性可以检测出实例对象是由哪一个构造函数产生的,即原型对象与构造函数之间的关系，
+  但是这个原型对象是可以改的，有可能原型对象修改了，但是constructor没有一起修改，这个时候就会检测出错
+
+  e.g: 'string'.constructor === String  // true
+
+  String,
+  Boolean,
+  Array,
+  HtmlDocument,
+  window,
+  Number,
+  Function,
+  Date,
+  RegExp,
+  Error
+*/
