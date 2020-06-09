85de912334a61d5f48c4e72950f15097d375c772
yangxinchi <1451845265@qq.com>
2020-06-09 10:33:38 +0800

    feat: 添加all. finally

--- handwrite/promise.js
+++ handwrite/promise.js
@@ -27,14 +27,14 @@ class _Promise {
 
     // 成功和失败的状态转换处理
     const fulfill = (value) => {
-      if(this.state === STATE.PENDING) {
+      if (this.state === STATE.PENDING) {
         this.value = value;
         this.state = STATE.FULFILLED;
-        this.fulfilledCallbacks.forEach(cb=> cb());
+        this.fulfilledCallbacks.forEach(cb => cb());
       }
     }
     const reject = (reason) => {
-      if(this.state === STATE.PENDING) {
+      if (this.state === STATE.PENDING) {
         this.reason = reason;
         this.state = STATE.REJECTED;
         this.rejectedCallbacks.forEach(cb => cb());
@@ -51,10 +51,10 @@ class _Promise {
 
   // 1. 根据状态执行成功的回调或者失败的回调
   // 2. then 函数内 return 一个新的 promise 实现链式调用 onFuifilled 和 onRejected 传给新的 fulfill 和 reject
-  then(onFulfilled, onRejected) {
+  then (onFulfilled, onRejected) {
     return new _Promise((fulfill, reject) => {
       // 实现异步，状态为 pending 时，fulfill 和 reject 的回调存储起来
-      if(this.state === STATE.PENDING) {
+      if (this.state === STATE.PENDING) {
         this.fulfilledCallbacks.push(() => {
           onFulfilled(this.value);
         })
@@ -64,12 +64,12 @@ class _Promise {
       }
 
       // 执行成功的回调
-      if(this.state === STATE.FULFILLED) {
+      if (this.state === STATE.FULFILLED) {
         fulfill(onFulfilled(this.value));
       }
 
       // 执行失败的回调
-      if(this.state === STATE.REJECTED) {
+      if (this.state === STATE.REJECTED) {
         reject(onRejected(this.reason));
       }
     })
@@ -77,21 +77,46 @@ class _Promise {
 
   // 1, 返回是一个promise，谁先完成就返回谁
   // 2. 输入是一组promise
-  race(promises){
+  race (promises) {
     const promiseArr = Array.from(promises);
     //
-    return  _Promise((resolve, reject) => {
-      if(promiseArr.length === 0){
+    return _Promise((resolve, reject) => {
+      if (promiseArr.length === 0) {
         return;
       } else {
         promiseArr.forEach(promise => {
-          promise.then(resolve,reject);
+          promise.then(resolve, reject);
         })
       }
     })
   }
-  finally() {
-    
+
+  // all
+  all (promises) {
+    return new _Promise((resolve, reject) => {
+      let result = [];
+      let promiseIndex = 0;
+      let promiseLength = promises.length;
+      for (let p of promises) {
+        // .then中收集数据，并添加.catch
+        Promise.resolve(p).then((res) => {
+          promiseIndex = promiseIndex + 1;
+          result[promiseIndex] = res;
+          if(promiseIndex === promiseLength){
+            return resolve(results);
+          }
+        })
+          .catch(reject);
+      }
+    })
+  }
+
+  // finally 表示不管resolve还是reject,finally中的回调都应该会调用。
+  finally(callback){
+    return this.then(
+      value => _Promise.resolve(callback).then(() => value),
+      reason => _promise.resolve(callback).then(() => reason)
+    )
   }
 
 }
