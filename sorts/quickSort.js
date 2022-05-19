539cc70324f8fec5f23a472d6272598700e0ab71
xinchi <xinchi.yang@dreampix.cn>
2022-05-19 10:10:20 +0800

    feat: 更改注释

--- sorts/quickSort.js
+++ sorts/quickSort.js
@@ -8,27 +8,27 @@
 
 const array = [43, 20, 17, 13, 44, 28, 14, 23, 15, 44];
 
-// 分区(寻找哨兵位置)
+// 分区
 const partition = (arr, low, high) => {
-  // 默认第一个作为哨兵
+  // 默认第一个作为分界点
   let povitkey = arr[low];
   
   while(low < high) {
-    // 从后往前，直到找到比哨兵小的 元素: array[high]
+    // 从后往前，直到找到比分界点小的 元素: array[high]
     while( low < high && arr[high] >= povitkey) {
       high--;
     }
-    // 比枢轴小的元素放到arr[low]
+    // 比分界点小的元素放到arr[low]
     arr[low] = arr[high];
     
-    // 从前往后，直到找到第一个比枢轴大的元素
+    // 从前往后，直到找到第一个比分界点大的元素
     while(low < high && arr[low] <= povitkey) {
       low++;
     }
-     // 比枢轴大的元素放到arr[high]
+     // 比分界点大的元素放到arr[high]
      arr[high] = arr[low];
   }
-  // 哨兵（枢轴）位置
+  // 分界点（分界点）位置
   arr[low] = povitkey;
 
   // 返回正确位置
@@ -43,7 +43,7 @@ const partition = (arr, low, high) => {
  * **/
 const quickSort = (arr=[], low, high) => {
   if(low < high) {
-    // 分区(哨兵位置)
+    // 分区(分界点位置)
     let povitkeyLoc = partition(arr, low, high);
     quickSort(arr, low, povitkeyLoc - 1);
     quickSort(arr, povitkeyLoc + 1, high);
